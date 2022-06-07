01ef21038aac4225cdc6355cf2a57b2ed37d9935
Shpetim Aliu <106196770+shpetimaliu@users.noreply.github.com>
2022-06-08 01:22:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; 
-
+let highscore = 0;
 
 
 // ! Eventi i shtuar me efekt për klikim
@@ -30,7 +30,11 @@ else if(guess === secretNumber)   {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem'; 
-
+    
+    if  (score > highscore){
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+    }
 
 } else if(guess > secretNumber) {
     
