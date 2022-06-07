7852c83e81e87922a6cedc96ad34da58284d4a70
Shpetim Aliu <106196770+shpetimaliu@users.noreply.github.com>
2022-06-08 00:54:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 'use strict';
-/*
-console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🏆 Rezultati i Saktë';
-document.querySelector('.number').textContent = 13; 
-document.querySelector('.score').textContent = 10; 
-
-console.log(document.querySelector('.guess').value);  
-
-document.querySelector('.guess').value = 20; 
-*/
 // ! Gjeneratori i numrave random
 // ! Random number generator
 
